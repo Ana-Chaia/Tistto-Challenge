7b2f426087a00fa373c28f9b44fc2c538e475dc3
Ana Chaia <anacarolinarsr@gmail.com>
2024-08-06 01:26:48 -0300

    writing django models and migrate changes to database PostgreSQL

--- todo_project/authenticationapp/models.py
+++ todo_project/authenticationapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    password = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
