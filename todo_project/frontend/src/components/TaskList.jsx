36b6aae84375ab71c8a42b0fc088c92ef8aaa593
Ana Chaia <anacarolinarsr@gmail.com>
2024-08-06 23:06:38 -0300

    changes on frontend components

--- todo_project/frontend/src/components/TaskList.jsx
+++ todo_project/frontend/src/components/TaskList.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import { AiFillDelete } from 'react-icons/ai'
-import { TbEdit } from 'react-icons/tb'
+import React from 'react';
 
-const TaskList = ({ todos, delTodo, complete_todo }) => {
+const TaskList = ({ todos, delTodo, editTodo, completeTodo, toggle, update_todo, todoId, task, setTask, taskRef, setToggle }) => {
   return (
-    <div className="todo-list">
-      {todos.map((todo, index) => (
-        <div className="todo-list-item" key={index}>
-          <div className="task">
-            <input type="checkbox" onChange={(e) => complete_todo(e, todo.id, todo)} />
-            <p id="t_task" className={todo.status === "Completed" ? "strike" : ""}>{todo.task}</p>
-          </div>
-          <div className="btn-container">
-            <div className="edit">
-              <TbEdit size={25} />
-            </div>
-            <div className="del">
-              <AiFillDelete size={25} onClick={() => delTodo(todo.id)} />
-            </div>
-          </div>
+    <div className="task-list">
+      {todos.map(todo => (
+        <div key={todo.id} className="task-item">
+          <p>{todo.text}</p>
+          <button onClick={() => completeTodo(todo.id)}>Complete</button>
+          <button onClick={() => editTodo(todo.id)}>Edit</button>
+          <button onClick={() => delTodo(todo.id)}>Delete</button>
         </div>
       ))}
+      
+      {toggle && (
+        <div className="modal-container">
+          <div className="modal">
+            <h1>Update Form</h1>
+            <form action="" onSubmit={(e) => { update_todo(e, todoId, task); setToggle(false); }}>
+              <input type="text" ref={taskRef} placeholder="Update Todo" value={task} onChange={(e) => setTask(e.target.value)} required />
+              <button id="add">Add</button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
